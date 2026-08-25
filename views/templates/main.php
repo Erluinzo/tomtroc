db6996f19426bd0f28cfc9f3606f8417854115f4
Onur <github@nx-lab.com>
2026-08-25 02:08:04 +0200

    Add site header with navigation and logo

--- views/templates/main.php
+++ views/templates/main.php
@@ -7,14 +7,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($title) ?> - TomTroc</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
     <header class="header">
-        <a class="header__brand" href="index.php">TomTroc</a>
-        <nav class="header__nav">
-            <a class="header__link" href="index.php">Accueil</a>
-        </nav>
+        <div class="container header__bar">
+            <a class="logo" href="index.php" aria-label="Tom Troc, accueil">
+                <span class="logo__mark" aria-hidden="true">TT</span>
+                <span class="logo__word">Tom Troc</span>
+            </a>
+
+            <nav class="nav nav--main" aria-label="Navigation principale">
+                <ul>
+                    <li><a href="index.php" aria-current="page">Accueil</a></li>
+                    <li><a href="#">Nos livres à l'échange</a></li>
+                </ul>
+            </nav>
+
+            <nav class="nav nav--user" aria-label="Navigation du compte">
+                <ul>
+                    <li><a href="#">Messagerie <span class="badge">1</span></a></li>
+                    <li><a href="#">Mon compte</a></li>
+                    <li><a href="#">Connexion</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
 
     <main class="main">
