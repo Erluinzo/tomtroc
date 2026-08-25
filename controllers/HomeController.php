5acdf37b96df5bb287f4c91e5aeddc8b5d8c6f48
Onur <github@nx-lab.com>
2026-08-25 05:26:50 +0200

    Build home page hero section

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -3,17 +3,10 @@
 //controller for the home page
 class HomeController
 {
-    //show the home page with a user from the database
+    //show the home page
     public function showHome(): void
     {
-        $userManager = new UserManager();
-        $user = $userManager->getUserById(1);
-
-        if (!$user) {
-            throw new Exception("Aucun membre à afficher.");
-        }
-
         $view = new View("Accueil");
-        $view->render("home", ['user' => $user]);
+        $view->render("home");
     }
 }
--- views/templates/home.php
+++ views/templates/home.php
@@ -1,8 +1,17 @@
 <?php
-//home page, needs a $user
+//home page
 ?>
-<section class="home">
-    <h1 class="home__title">Bienvenue sur TomTroc</h1>
-    <p class="home__text">Premier membre enregistré : <?= htmlspecialchars($user->getUsername()) ?></p>
-    <p class="home__text">Membre depuis le <?= htmlspecialchars($user->getCreatedAt()) ?></p>
+<section class="hero">
+    <div class="container hero__inner">
+        <div class="hero__text">
+            <h1 class="hero__title">Rejoignez nos lecteurs passionnés</h1>
+            <p class="hero__desc">Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.</p>
+            <a class="btn btn--primary" href="#">Découvrir</a>
+        </div>
+
+        <figure class="hero__media">
+            <img src="./img/hero.jpg" alt="Un lecteur installé au milieu de piles de livres" width="404" height="539">
+            <figcaption class="hero__credit">Hamza</figcaption>
+        </figure>
+    </div>
 </section>
