8e0932fa88da667f3fc1c92f84c2c4fc0d4f6960
Onur <github@nx-lab.com>
2026-08-25 12:47:54 +0200

    Add values section to home page

--- views/templates/home.php
+++ views/templates/home.php
@@ -60,3 +60,20 @@
 </section>
 
 <div class="banner"></div>
+
+<section class="values">
+    <div class="container">
+        <div class="values__inner">
+            <h2 class="values__title">Nos valeurs</h2>
+
+            <div class="values__text">
+                <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
+                <p>Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé.</p>
+                <p>Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
+            </div>
+
+            <p class="values__team">L'équipe Tom Troc</p>
+            <img class="values__heart" src="./img/heart.svg" alt="" width="100" height="88">
+        </div>
+    </div>
+</section>
